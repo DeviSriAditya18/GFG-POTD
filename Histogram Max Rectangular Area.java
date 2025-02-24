78b221f318bc5d9f05763502b00eb262be78ff5c
Aditya <devisriaditya17@gmail.com>
2025-02-25 00:43:56 +0530

    POTD: 25-02-2025

---
+++ Histogram Max Rectangular Area.java
@@ -0,0 +1,37 @@
+/*
+You are given a histogram represented by an array arr, where each element of the array denotes the height of the bars in the histogram. All bars have the same width of 1 unit.
+
+Your task is to find the largest rectangular area possible in the given histogram, where the rectangle can be formed using a number of contiguous bars.
+
+Examples:
+
+Input: arr[] = [60, 20, 50, 40, 10, 50, 60]
+Output: 100
+Explanation: We get the maximum by picking bars highlighted above in green (50, and 60). The area is computed (smallest height) * (no. of the picked bars) = 50 * 2 = 100.
+
+Input: arr[] = [3, 5, 1, 7, 5, 9]
+Output: 15
+Explanation:  We get the maximum by picking bars 7, 5 and 9. The area is computed (smallest height) * (no. of the picked bars) = 5 * 3 = 15.
+
+Input: arr[] = [3]
+Output: 3
+Explanation: In this example the largest area would be 3 of height 3 and width 1.
+*/
+
+class Solution{
+    public static int getMaxArea(int arr[]) {
+        // your code here
+        Stack<Integer> s=new Stack<>();
+        int ans=0;
+        for(int i=0;i<=arr.length;i++){
+            int c=(i==arr.length)?0:arr[i];
+            while(!s.isEmpty() && c<arr[s.peek()]){
+                int h=arr[s.pop()];
+                int w=(s.isEmpty())?i:i-s.peek()-1;
+                ans=Math.max(ans,h*w);
+            }
+            s.push(i);
+        }
+        return ans;
+    }
+}
