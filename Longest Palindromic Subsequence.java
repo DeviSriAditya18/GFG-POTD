22ee97047680041c043595eb58acdeb1b6d489cd
Aditya <devisriaditya17@gmail.com>
2025-03-07 15:38:54 +0530

    POTD: 07-03-2025

---
+++ Longest Palindromic Subsequence.java
@@ -0,0 +1,44 @@
+/*
+Given a string s, return the length of the longest palindromic subsequence.
+
+A subsequence is a sequence that can be derived from the given sequence by deleting some or no elements without changing the order of the remaining elements.
+
+A palindromic sequence is a sequence that reads the same forward and backward.
+
+Examples:
+
+Input: s = "bbabcbcab"
+Output: 7
+Explanation: Subsequence "babcbab" is the longest subsequence which is also a palindrome.
+
+Input: s = "abcd"
+Output: 1
+Explanation: "a", "b", "c" and "d" are palindromic and all have a length 1.
+
+Input: s = "agbdba"
+Output: 5
+Explanation: The longest palindromic subsequence is "abdba", which has a length of 5. The characters in this subsequence are taken from the original string "agbdba", and they maintain the order of the string while forming a palindrome. 
+*/
+
+class Solution {
+    public int longestPalinSubseq(String s) {
+        // code here
+        StringBuilder sb=new StringBuilder(s).reverse();
+        String s1=s;
+        String s2=sb.toString();
+        int n=s1.length();
+        int m=s2.length();
+        int dp[][]=new int[n+1][m+1];
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
