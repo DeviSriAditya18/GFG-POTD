1d2fe4846b171735a48584d0e5c0d6c71d8e4951
devisriaditya <devisri.bandaru@simgems.com>
2025-04-20 00:18:25 +0530

    POTD: 20-04-2025

---
+++ Find Only Repetitive Element from 1 to n-1.java
@@ -0,0 +1,30 @@
+/*
+Given an array arr[] of size n, filled with numbers from 1 to n-1 in random order. The array has only one repetitive element. Your task is to find the repetitive element.
+
+Note: It is guaranteed that there is a repeating element present in the array.
+
+Examples:
+
+Input: arr[] = [1, 3, 2, 3, 4]
+Output: 3 
+Explanation: The number 3 is the only repeating element.
+
+Input: arr[] = [1, 5, 1, 2, 3, 4]
+Output: 1  
+Explanation: The number 1 is the only repeating element.
+
+Input: arr[] = [1, 1]  
+Output: 1
+Explanation: The array is of size 2 with both elements being 1, making 1 the repeating element.
+*/
+
+class Solution {
+    public int findDuplicate(int[] arr) {
+        // code here
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            if(i+1!=arr[i]) return arr[i]; 
+        }
+        return 0;
+    }
+}
